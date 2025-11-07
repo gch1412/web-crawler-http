0ef31fc507d30e95c504face99b033a91689a97a
Gustavo Carvalho <gch1412@proton.me>
2025-11-07 13:36:38 -0300

    crawl page

--- crawl.js
+++ crawl.js
@@ -1,5 +1,31 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non-html response, content type: ${contentType} on page ${currentURL}`, )
+            return
+        }
+
+        console.log(await resp.text())
+
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page ${currentURL}`)
+    }
+
+
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -43,4 +69,4 @@ function normalizeURL(urlString) {
     return hostpath
 }
 
-module.exports = { normalizeURL, getURLsFromHTML }
+module.exports = { crawlPage, normalizeURL, getURLsFromHTML }
